64f7cbc685326230ed1424cf0b0905d5895707c1
Trevor <trevorme91@gmail.com>
2019-07-24 22:44:28 +0900

    With gifted-chat

--- App.js
+++ App.js
@@ -1,61 +1,48 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Image, TextInput, Button, FlatList } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  FlatList,
+} from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 import io from 'socket.io-client';
 
 const socket = io('http://121.254.173.45:3333');
+
+const ID = getRandomInt(0, 1000);
 
 export default function App() {
   const [input, setInput] = useState('hey');
   const [messages, setMessages] = useState([]);
 
-  const handleSubmit = () => {
-    socket.emit('new message', input);
-    setMessages(prev => [...prev, { username: 'React Native', message: input }]);
-    setInput('');
+  const onSend = (giftMessages = []) => {
+    socket.emit('new message', giftMessages[0]);
+    setMessages(prev => GiftedChat.append(prev, giftMessages));
   };
 
   useEffect(() => {
-    socket.on('new message', function(messageObj) {
-      setMessages(prev => [...prev, messageObj]);
+    socket.on('new message', function(data) {
+      setMessages(prev => GiftedChat.append(prev, data));
     });
   }, []);
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={{ height: 40 }}
-        placeholder="Type here to translate!"
-        onChangeText={text => setInput(text)}
-        value={input}
-      />
-      <Button
-        onPress={handleSubmit}
-        title="Send"
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
-      />
-      <FlatList
-        // data={[{ key: 'a' }, { key: 'b' }]}
-        data={messages}
-        renderItem={({ item: { username, message } }) => <Text>{`${username}: ${message}`}</Text>}
-      />
-    </View>
+    <GiftedChat
+      messages={messages}
+      onSend={onSend}
+      user={{
+        _id: ID,
+        name: 'React Native',
+        avatar: 'https://placeimg.com/140/140/any',
+      }}
+    />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-const Bananas = () => {
-  let pic = {
-    uri:
-      'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg',
-  };
-  return <Image source={pic} style={{ width: 193, height: 110 }} />;
-};
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
